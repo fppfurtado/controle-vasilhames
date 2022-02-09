53c19d6d202e88fc81da1baaa7f6ef809782a774
Fernando Furtado <fernandofurtado92@hotmail.com>
2022-02-09 20:07:09 -0300

    função salvarDocumento criada para o firestore

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var docRef = firebase.firestore().collection("teste").doc("cZ494FOwMgDBuwNRFHeg");
+// document.addEventListener('DOMContentLoaded', function() {
+//     var docRef = firebase.firestore().collection("teste").doc("cZ494FOwMgDBuwNRFHeg");
 
-    docRef.get().then((doc) => {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error);
+//     docRef.get().then((doc) => {
+//         if (doc.exists) {
+//             console.log("Document data:", doc.data());
+//         } else {
+//             // doc.data() will be undefined in this case
+//             console.log("No such document!");
+//         }
+//     }).catch((error) => {
+//         console.log("Error getting document:", error);
+//     });
+
+//   });
+
+//const db = firebase.firestore();
+
+function salvarDocumento(colecao, documento) {
+    
+    firebase.firestore().collection(colecao).add(documento)
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
     });
 
-  });
-
-  function teste() {
-      console.log("gravando...");
-  }
+}
