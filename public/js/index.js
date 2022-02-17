88a7d89884d4b2d4bac4167cd08b87e318ce23de
Fernando Furtado <fernandofurtado92@hotmail.com>
2022-02-17 07:52:03 -0300

    atualizada função de gravação de registro com verificação de duplicidade

--- public/js/index.js
+++ public/js/index.js
@@ -16,14 +16,53 @@
 
 //const db = firebase.firestore();
 
-function salvarDocumento(colecao, documento) {
+function salvarDocumento(colecao, documento, id) {
+
+    // firebase.firestore().collection(colecao).doc(id).set(documento)
+    // .then((docRef) => {
+    //     console.log("Document written with ID: ", id);
+    // })
+    // .catch((error) => {
+    //     console.error("Error adding document: ", error);
+    // });
     
-    firebase.firestore().collection(colecao).add(documento)
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
+    firebase.firestore().collection(colecao).doc(id)
+    .get()
+    .then((documentSnapshot) => {
+        console.log(documentSnapshot);
+        if(!documentSnapshot.exists) {
+            firebase.firestore().collection(colecao).doc(id).set(documento)
+            .then((docRef) => {
+                console.log("Document written with ID: ", id);
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+            });  
+        }
+        else {
+            alert("Registro já cadastrado");
+        }
     })
     .catch((error) => {
-        console.error("Error adding document: ", error);
+        console.log("Error getting documents: ", error);
     });
+    
+}
+
+function verificarDuplicidadeRomaneio(documento, id) {
+    
+    firebase.firestore().collection("romaneios").doc(id)
+    .get()
+    .then((documentSnapshot) => {
+        // querySnapshot.forEach((doc) => {
+        //     // doc.data() is never undefined for query doc snapshots
+        //     console.log(doc.id, " => ", doc.data());
+        // });
+        console.log(documentSnapshot);
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
 
 }
