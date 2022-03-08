1e8ba175195ea15b2ece5402e6ffcc743beef11a
Fernando Furtado <fernandofurtado92@hotmail.com>
2022-03-08 06:29:32 -0300

    registro de movimento e inventario parcialmente implementado

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,45 @@
 const inventario = {
     qtdVasilhamesCheiosDiaAnterior: 0,
     qtdVasilhamesVaziosDiaAnterior: 0,
-    qtdVasilhamesQuebradosDiaAnterior: 0,
+    qtdVasilhamesInutizadosDiaAnterior: 0,
     qtdVasilhamesCheiosDiaAtual: 0,
     qtdVasilhamesVaziosDiaAtual: 0,
-    qtdVasilhamesQuebradosDiaAtual: 0
+    qtdVasilhamesInutizadosDiaAtual: 0
 };
 
-document.addEventListener("DOMContentLoaded", function() {
+function carregarInventario(data) {
 
-    console.log("[DOMContentLoaded]");
-
+    console.log("[VERIFICAR INVENTARIO]");
+console.log("data: "+data);
     // consulta se já existe o inventário do dia
     firebase.firestore().collection("inventarios")
-    .where("data", "==", new Date())
+    .where("data", "==", data)
     .get()
     .then((querySnapshot) => {
-        //console.log(querySnapshot);
+        console.log("querySnapshot");
+        console.log(querySnapshot);
         // se não existir...
         if(querySnapshot.empty) {
 
+            console.log("querySnapshot vazia");
+            console.log("data: " + data);
             //inventario.data = firebase.firestore.Timestamp.fromDate(new Date());
-            inventario.data = getTodayDate();
+            console.log("inventario: "+inventario);
+            inventario.data = data;
             
             // carrega o inventario do dia anterior...
             carregarDocumentos("inventarios")
             .then((querySnapshot) => {
 
-                querySnapshot.query.where("data", "==", getLastDayDate(inventario.data))
+                querySnapshot.query.where("data", "==", getLastDayDate(data))
                 .get()
                 .then((querySnapshot) => {
 
                     querySnapshot.forEach((doc) => {
 
                         let invent = doc.data();
+
+                        console.log(doc);
 
                         console.log("invent:");
                         console.log(invent);
@@ -46,8 +52,8 @@ document.addEventListener("DOMContentLoaded", function() {
                         inventario.qtdVasilhamesVaziosDiaAnterior = 
                             invent.qtdVasilhamesVaziosDiaAnterior + invent.qtdVasilhamesVaziosDiaAtual;
 
-                        inventario.qtdVasilhamesQuebradosDiaAnterior = 
-                            invent.qtdVasilhamesQuebradosDiaAnterior + invent.qtdVasilhamesQuebradosDiaAnterior;
+                        inventario.qtdVasilhamesInutizadosDiaAnterior = 
+                            invent.qtdVasilhamesInutizadosDiaAnterior + invent.qtdVasilhamesInutizadosDiaAnterior;
 
                         //criarDocumento("inventarios", inventario);
 
@@ -67,13 +73,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
                 let invent = doc.data();
 
+                console.log(doc);
+                console.log(invent);
+
+                //return invent;
+
                 inventario.data = invent.data;
                 inventario.qtdVasilhamesCheiosDiaAnterior = invent.qtdVasilhamesCheiosDiaAnterior;
                 inventario.qtdVasilhamesCheiosDiaAtual = invent.qtdVasilhamesCheiosDiaAtual;
                 inventario.qtdVasilhamesVaziosDiaAnterior = invent.qtdVasilhamesVaziosDiaAnterior;
                 inventario.qtdVasilhamesVaziosDiaAtual = invent.qtdVasilhamesVaziosDiaAtual;
-                inventario.qtdVasilhamesQuebradosDiaAnterior = invent.qtdVasilhamesQuebradosDiaAnterior;
-                inventario.qtdVasilhamesQuebradosDiaAtual = invent.qtdVasilhamesQuebradosDiaAtual;
+                inventario.qtdVasilhamesInutizadosDiaAnterior = invent.qtdVasilhamesInutizadosDiaAnterior;
+                inventario.qtdVasilhamesInutizadosDiaAtual = invent.qtdVasilhamesInutizadosDiaAtual;
 
             })
 
@@ -86,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Error on getting inventarios: "+error);
     })
 
-  })
+}
 
 function criarDocumento(colecao, documento) {
 
@@ -101,6 +112,7 @@ function criarDocumento(colecao, documento) {
                 if(!documentSnapshot.exist) {
                 firebase.firestore().collection(colecao).doc(documento.id).set(documento)
                     .then((docRef) => {
+                        return docRef;
                         console.log("Document written with ID: ", documento.id);
                     })
                     .catch((error) => {
@@ -124,6 +136,7 @@ function criarDocumento(colecao, documento) {
         firebase.firestore().collection(colecao).add(documento)
         .then((docRef) => {
             console.log("documento registrado: "+docRef.id);
+            return docRef;
         })
         .catch((error) => {
             console.log("erro no registro do documento: "+error);
@@ -131,42 +144,6 @@ function criarDocumento(colecao, documento) {
 
     }
     
-}
-
-function formatarData(data, formato = "br") {
-
-    console.log("[FORMATAR DATA]");
-
-    let epochTimestamp = data.seconds + "000";
-    let dataJS = new Date(eval(epochTimestamp));
-    //let dia = formatarZeroAEsquerda(dataJS.getDate());
-    let ano = dataJS.getFullYear();
-    let mes = String(dataJS.getMonth()+1).padStart(2, "0");
-    let dia = String(dataJS.getDate()).padStart(2, "0");
-    let horas = dataJS.getHours();
-    let minutos = dataJS.getMinutes();
-    //let mes = formatarZeroAEsquerda(dataJS.getMonth()+1);
-    
-    let dataFormatada = formato == "br" ? 
-    dia  + "/" + mes + "/" + ano : 
-    ano + "-" + mes + "-" + dia + "T" + horas + ":" + minutos; 
-
-    return dataFormatada;
-
-}
-
-function getLastDayDate(date) {
-    return new Date(date.getTime() - 24*60*60*1000);
-}
-
-function getTodayDate() {
-
-    let hoje = new Date();
-    let ano = hoje.getFullYear().toString();
-    let mes = String(hoje.getMonth()+1).padStart(2, "0");
-    let dia = String(hoje.getDate()).padStart(2, "0");
-
-    return new Date(ano + "-" + mes + "-" + dia + " 00:00");
 }
 
 function carregarDocumentos(colecao, id = null) {
@@ -232,6 +209,42 @@ function excluirDocumento(colecao, id) {
     });
 }
 
+function formatarData(data, formato = "br") {
+
+    console.log("[FORMATAR DATA]");
+
+    let epochTimestamp = data.seconds + "000";
+    let dataJS = new Date(eval(epochTimestamp));
+    //let dia = formatarZeroAEsquerda(dataJS.getDate());
+    let ano = dataJS.getFullYear();
+    let mes = String(dataJS.getMonth()+1).padStart(2, "0");
+    let dia = String(dataJS.getDate()).padStart(2, "0");
+    let horas = dataJS.getHours();
+    let minutos = dataJS.getMinutes();
+    //let mes = formatarZeroAEsquerda(dataJS.getMonth()+1);
+    
+    let dataFormatada = formato == "br" ? 
+    dia  + "/" + mes + "/" + ano : 
+    ano + "-" + mes + "-" + dia + "T" + horas + ":" + minutos; 
+
+    return dataFormatada;
+
+}
+
+function getLastDayDate(date) {
+    return new Date(date.getTime() - 24*60*60*1000);
+}
+
+function getTodayDate() {
+
+    let hoje = new Date();
+    let ano = hoje.getFullYear().toString();
+    let mes = String(hoje.getMonth()+1).padStart(2, "0");
+    let dia = String(hoje.getDate()).padStart(2, "0");
+
+    return new Date(ano + "-" + mes + "-" + dia + " 00:00");
+}
+
 function getUrlParameter(sParam) {
 
     console.log("[GET URL PARAMENTER]");
@@ -249,4 +262,14 @@ function getUrlParameter(sParam) {
         }
     }
     return false;
-};
+}
+
+function atualizarInventario(qtdVasilhamesCheiosDiaAtual, qtdVasilhamesVaziosDiaAtual, qtdVasilhamesInutizadosDiaAtual) {
+
+    inventario.qtdVasilhamesCheiosDiaAtual += qtdVasilhamesCheiosDiaAtual;
+    inventario.qtdVasilhamesVaziosDiaAtual += qtdVasilhamesVaziosDiaAtual;
+    inventario.qtdVasilhamesInutizadosDiaAtual += qtdVasilhamesInutizadosDiaAtual;
+    
+    atualizarDocumento("inventarios", inventario, inventario.id);
+
+}
