c1673d732a18e2072090e0320818258c282cda0a
Fernando Furtado <fppfurtado@gmail.com>
2023-05-09 20:23:16 -0300

    ajusta configurações webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/app.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'public'),
   },
 };
